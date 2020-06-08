f62223d93191de765d695678e3763d8ebe1710e2
jamesncox <jamesncox@yahoo.com>
2020-06-08 11:11:48 -0500

    Add Item resource migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  
   namespace :api do
     namespace :v1 do
       resources :users
       resources :stores
+      resources :items
 
        # sessions CSRF-TOKEN route
        get '/auth_check' => 'sessions#auth_check'
