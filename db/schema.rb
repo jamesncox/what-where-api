81bcaa89bd39e7734e7d37a35432ab180e0e1a4a
jamesncox <jamesncox@yahoo.com>
2020-06-08 17:47:19 -0500

    Comment out sqlite3 gem

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_06_08_161038) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.integer "quantity"
