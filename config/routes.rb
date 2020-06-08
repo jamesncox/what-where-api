6cd3054f16cd058bbcc944b6ec7d28c9c0c2b149
jamesncox <jamesncox@yahoo.com>
2020-06-08 11:08:21 -0500

    Add Store resource migration

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
+      resources :stores
 
        # sessions CSRF-TOKEN route
        get '/auth_check' => 'sessions#auth_check'
