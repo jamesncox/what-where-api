c91149cb1f9d8c2e6155cac8e52b4c214f3f9a32
jamesncox <jamesncox@yahoo.com>
2020-06-14 12:23:13 -0500

    Add Items and Stores controller, update Routes

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::ItemsController < ApplicationController
-
     def index
         @items = Item.all 
         render json: @items, include: :store, status: 200
@@ -11,8 +10,28 @@ class Api::V1::ItemsController < ApplicationController
     end
 
     def create 
-        @item = Item.create(item_params)
-        render json: @item, include: :store, status: 200
+        @item = Item.new(item_params)
+        @store = Store.find_by(id: params[:store_id])
+        if @item.save
+            render json: @item, status: 200
+        else
+            render json: { errors: @item.errors.full_messages}, status: 400
+        end
+    end 
+
+    def update
+        @item = Item.find_by(id: params[:id])
+        @item.update(item_params)
+        render json: @item, status: 200
     end
 
+    def destroy
+        @item.destroy
+        head :no_content
+    end
+
+    private 
+        def item_params
+            params.require(:item).permit(:name, :price, :quantity, :store_id)
+        end
 end
