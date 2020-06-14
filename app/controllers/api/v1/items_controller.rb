1cdecc15126b0f75b2b19600c1f567bbcb711551
jamesncox <jamesncox@yahoo.com>
2020-06-14 12:09:43 -0500

    Updatre Items Controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,2 +1,18 @@
 class Api::V1::ItemsController < ApplicationController
+
+    def index
+        @items = Item.all 
+        render json: @items, include: :store, status: 200
+    end
+
+    def show
+        @item = Item.find_by(id: params[:id])
+        render json: @item, include: :store, status: 200
+    end
+
+    def create 
+        @item = Item.create(item_params)
+        render json: @item, include: :store, status: 200
+    end
+
 end
