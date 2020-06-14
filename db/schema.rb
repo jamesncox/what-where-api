b7ece33ba3559587a8c57b662f256f2c289095a6
jamesncox <jamesncox@yahoo.com>
2020-06-14 12:26:33 -0500

    Add UserId to Store

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_161038) do
+ActiveRecord::Schema.define(version: 2020_06_14_172351) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -27,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_06_08_161038) do
     t.string "color"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
