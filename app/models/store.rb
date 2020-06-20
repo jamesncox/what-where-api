1cd58ae46023d0f9c603f969143f151d0b290ef0
jamesncox <jamesncox@yahoo.com>
2020-06-20 14:21:36 -0500

    Update Store custom validations

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,7 +2,7 @@ class Store < ApplicationRecord
     belongs_to :user
     has_many :items
 
-    validates :name, presence: { message: "%{value} is missing" }
+    validates :name, presence: { message: "%{value} of store is missing" }
     validates :store_type, presence: { message: "%{value} is missing" }
-    validates :color, presence: { message: "%{value} of header is missing" }
+    validates :color, presence: { message: "%{value} of header not selected" }
 end
