5122b6d11638d34d9cab113eacb5b99f65467180
jamesncox <jamesncox@yahoo.com>
2020-06-27 15:37:19 -0500

    Update cors.rb with correct origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,31 +8,23 @@
 Rails.application.config.action_controller.forgery_protection_origin_check = false
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  # allow do
-  #   origins 'https://buy-where.netlify.app/'
-
-  #   resource '*',
-  #     headers: :any,
-  #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
-  #     credentials: true
-  # end
-
-  # allow do
-  #   origins 'http://localhost:3001'
-
-  #   resource '*',
-  #     headers: :any,
-  #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
-  #     credentials: true
-  # end
-
+  
   allow do
-    origins '*'
+    origins 'https://buy-where.netlify.app/'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-      # credentials: true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+
+  allow do
+    origins 'http://localhost:3001'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 
 end
