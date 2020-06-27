47dbfa7ac058cf22233937fce28a30ff40765ad1
jamesncox <jamesncox@yahoo.com>
2020-06-27 11:55:19 -0500

    Update cors.rb / remove localhost for now

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,9 +8,17 @@
 Rails.application.config.action_controller.forgery_protection_origin_check = false
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'https://buy-where.netlify.app/'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
 
   # allow do
-  #   origins 'https://buy-where.netlify.app/'
+  #   origins 'http://localhost:3001'
 
   #   resource '*',
   #     headers: :any,
@@ -18,12 +26,4 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   #     credentials: true
   # end
 
-  allow do
-    origins ['http://localhost:3001', 'https://buy-where.netlify.app/']
-
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
-  end
 end
