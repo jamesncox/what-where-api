ba27f695426973e74d5f31a2d06861f4ece42487
jamesncox <jamesncox@yahoo.com>
2020-06-27 18:01:22 -0500

    Add buy-where back to cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.action_controller.forgery_protection_origin_check = fal
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   
   allow do
-    origins 'https://buywhere.netlify.app/'
+    origins 'https://buy-where.netlify.app/'
 
     resource '*',
       headers: :any,
