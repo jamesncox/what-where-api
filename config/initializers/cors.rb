1fc54319831460ee970b55e59f73698394a7ec16
jamesncox <jamesncox@yahoo.com>
2020-06-27 15:29:03 -0500

    Add allow Wildcard * temporarily

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,13 +17,22 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   #     credentials: true
   # end
 
+  # allow do
+  #   origins 'http://localhost:3001'
+
+  #   resource '*',
+  #     headers: :any,
+  #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
+  #     credentials: true
+  # end
+
   allow do
-    origins 'http://localhost:3001'
+    origins '*'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      # credentials: true
   end
 
 end
