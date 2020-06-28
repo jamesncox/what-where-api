1f9fe0b74cc69b73c9a6577b9d521990df9046d0
jamesncox <jamesncox@yahoo.com>
2020-06-28 14:28:03 -0500

    Reset config.ru and move cors to config/application

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,39 @@ Bundler.require(*Rails.groups)
 
 module WhatWhereApi
   class Application < Rails::Application
+
+    Rails.application.config.action_controller.forgery_protection_origin_check = false
+
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+
+    allow do
+      origins 'https://buy-where.vercel.app/'
+
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+    end
+    
+    allow do
+      origins 'https://buy-where.netlify.app/'
+
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+    end
+
+    allow do
+      origins 'http://localhost:3001'
+
+      resource '*',
+        headers: :any,
+        methods: [:get, :post, :put, :patch, :delete, :options, :head],
+        credentials: true
+    end
+
+  end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
